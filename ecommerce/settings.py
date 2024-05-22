a7b4db054fedfe9333ef31b32cd55cb5407951fd
Harissh0110 <Harissh0110@gmail.com>
2024-05-22 23:09:43 +0530

    static file update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -155,10 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
 
-
-MEDIA_ROOT = BASE_DIR / 'static/images'
 
 
 # Default primary key field type
